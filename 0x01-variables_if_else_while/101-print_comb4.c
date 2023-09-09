a24e935b5c75e6f103b0b625ea56b132beaec1d8
Tiedaman33 <orinabravin6@gmail.com>
2023-09-09 04:31:20 -0700

    print 3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,22 +6,20 @@
  */
 int main(void)
 {
-	int h = '0';
-	int t = '0';
-	int o = '0';
+	int h, t, o;
 
-	for (h = '0'; h <= 9; h++)
+	for (h = 48; h <= 58; h++)
 	{
-		for (t = '0'; t <= '9'; t++)
+		for (t = 49; t <= 58; t++)
 		{
-			for (o = '0'; o <= '9'; o++)
+			for (o = 50; o <= 58; o++)
 			{
-				if (!((o == t) || (t == h) || (t > o) || (h > t)))
+				if (o > t && t > h)
 				{
 					putchar(h);
 					putchar(t);
 					putchar(o);
-					if (!(o == '9' && h == '7' && t == '8'))
+					if (h != 55 && t != 56)
 					{
 						putchar(',');
 						putchar(' ');
