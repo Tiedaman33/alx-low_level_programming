0a2083021caa41efd3853940195f013971eb5ced
Tiedaman33 <orinabravin6@gmail.com>
2023-09-26 15:50:01 -0700

    ile that contains the password for the crackme2 executable.

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string fucntion that sets value of pointer to char
+ * @s: input
+ * @to: input
+ *
+ * Return: always 0 (sucess)
+ */
+void set_string(char **s, char *to);
+{
+
