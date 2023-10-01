41c4ad4bf2a6d1b9196517cc84636e949c7b73e3
Tiedaman33 <orinabravin6@gmail.com>
2023-10-01 14:47:55 -0700

    give change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main -  the main function
+ * @argc: argument
+ * @argv: argment
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	int cents;
+	int coins[] = {25, 10, 5, 2, 1};
+	int num_coins = sizeof(coins) / sizeof(coins[0]);
+	int min_coins;
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 0; i < num_coins; i++)
+	{
+		while (cents >= coins[i])
+		{
+			cents -= coins[i];
+			min_coins++;
+		}
+	}
+
+	printf("%d\n", min_coins);
+	return (0);
+}
