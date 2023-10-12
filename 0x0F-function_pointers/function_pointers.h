55f137bdd76fc36c791904f3eb0885ec68d04234
Tiedaman33 <orinabravin6@gmail.com>
2023-10-12 05:21:34 -0700

    function pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -9,5 +9,7 @@ int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int main(int __attribute__((__unused__)) argc, char *argv[]);
+void print_opcodes(int num_bytes);
+int main(int argc, char *argv[]);
 
 #endif
