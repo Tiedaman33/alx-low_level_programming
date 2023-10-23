95a58ee87a98ee78283889bccae8fa5520aaaf1e
Tiedaman33 <orinabravin6@gmail.com>
2023-10-23 05:30:05 -0700

    length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - return numbe rof elements
+ * @h: pointer to list
+ * Return:  count
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
