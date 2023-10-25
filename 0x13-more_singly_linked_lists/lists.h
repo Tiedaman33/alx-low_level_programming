726c19872be3b8625bfe91235623c4787decf119
Tiedaman33 <orinabravin6@gmail.com>
2023-10-25 10:47:23 -0700

    deletes a node in a linked list at a certain index

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,7 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 size_t print_listint(const listint_t *h);
 int _putchar(char c);
 
