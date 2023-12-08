7a10cf76c91f5dbeab37db96c6a7145f04aa47ad
Tiedaman33 <orinabravin6@gmail.com>
2023-12-08 12:28:00 -0800

    my commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,7 +10,7 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t node_count = 0;
 	const dlistint_t *current = h;
 
-	while (current != NULL) 
+	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		node_count++;
